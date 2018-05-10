d45944c4773393a363100e20708b6131de7b29ac
P2000114125 <giovanni.visona@espedia.it>
2018-05-10 15:47:43 +0000

    Added working networkgraph

--- webapp/controller/WorkOrders.controller.js
+++ webapp/controller/WorkOrders.controller.js
@@ -60,6 +60,94 @@ sap.ui.define([
 
 			this.byId("viewer").setVisible(false);
 			this.manageDetailToolbar("None");
+
+			///////////////////////////
+			// Setup for NetworkGraph
+			///////////////////////////
+			this._oModel = new sap.ui.model.json.JSONModel("mockData/graph.json");
+			this._oModel.setDefaultBindingMode(sap.ui.model.BindingMode.OneWay);
+
+			this._sTopParent = this.STARTING_PROFILE;
+			this._mExplored = [this._sTopParent];
+
+			this._graph = this.getView().byId("graph").setModel(this._oModel);
+
+			this._setFilter();
+
+			this._graph.attachEvent("beforeLayouting", function(oEvent) {
+				// nodes are not rendered yet (bOutput === false) so their invalidation triggers parent (graph) invalidation
+				// which results in multiple unnecessary loading
+				this._graph.preventInvalidation(true);
+				this._graph.getNodes().forEach(function(oNode) {
+
+					//oNode.attachPress(this.onNodePress, this);
+
+					var oExpandButton, oDetailButton, oUpOneLevelButton,
+						sIsParent = this._getCustomDataValue(oNode, "is-parent"),
+						sParent;
+
+					oNode.removeAllActionButtons();
+
+					if (!sIsParent) {
+						// employees without team - hide expand buttons
+						oNode.setShowExpandButton(false);
+					} else {
+						if (this._mExplored.indexOf(oNode.getKey()) === -1) {
+
+							// managers with team but not yet expanded
+							// we create custom expand button with dynamic loading
+							oNode.setShowExpandButton(false);
+
+							// this renders icon marking collapse status
+							oNode.setCollapsed(true);
+							oExpandButton = new sap.suite.ui.commons.networkgraph.ActionButton({
+								title: "Expand",
+								icon: "sap-icon://sys-add",
+								press: function() {
+									oNode.setCollapsed(false);
+									this._loadMore(oNode.getKey());
+								}.bind(this)
+							});
+							oNode.addActionButton(oExpandButton);
+						} else {
+							// manager with already loaded data - default expand button
+							oNode.setShowExpandButton(true);
+						}
+					}
+
+					// add detail link -> custom popover
+					oDetailButton = new sap.suite.ui.commons.networkgraph.ActionButton({
+						title: "Detail",
+						icon: "sap-icon://hint",
+						press: function(oEvent) {
+							this._openDetail(oNode, oEvent.getParameter("buttonElement"));
+						}.bind(this)
+					});
+					oNode.addActionButton(oDetailButton);
+
+					// if current equipment is root we can add 'up one level'
+					if (oNode.getKey() === this._sTopParent) {
+						sParent = this._getCustomDataValue(oNode, "parent");
+						if (sParent) {
+							oUpOneLevelButton = new sap.suite.ui.commons.networkgraph.ActionButton({
+								title: "Up one level",
+								icon: "sap-icon://arrow-top",
+								press: function() {
+									var aSuperVisors = oNode.getCustomData().filter(function(oData) {
+											return oData.getKey() === "parent";
+										}),
+										sParent = aSuperVisors.length > 0 && aSuperVisors[0].getValue();
+
+									this._loadMore(sParent);
+									this._sTopParent = sParent;
+								}.bind(this)
+							});
+							oNode.addActionButton(oUpOneLevelButton);
+						}
+					}
+				}, this);
+				this._graph.preventInvalidation(false);
+			}.bind(this));
 		},
 
 		onListSelectionChange: function(oEvent) {
@@ -205,6 +293,120 @@ sap.ui.define([
 			if (this._viewerContentResource) {
 				this.byId("viewer").addContentResource(this._viewerContentResource);
 			}
+		},
+
+		search: function(oEvent) {
+			var sKey = oEvent.getParameter("key");
+			if (sKey) {
+				this._mExplored = [sKey];
+				this._sTopParent = sKey;
+				this._graph.destroyAllElements();
+				this._setFilter();
+				oEvent.bPreventDefault = true;
+			}
+		},
+
+		suggest: function(oEvent) {
+			var aSuggestionItems = [],
+				aItems = this._oModel.getData().nodes,
+				aFilteredItems = [],
+				sTerm = oEvent.getParameter("term");
+			sTerm = sTerm ? sTerm : "";
+			aFilteredItems = aItems.filter(function(oItem) {
+				var sTitle = oItem.title ? oItem.title : "";
+				return sTitle.toLowerCase().indexOf(sTerm.toLowerCase()) !== -1;
+			});
+			aFilteredItems.sort(function(oItem1, oItem2) {
+				var sTitle = oItem1.title ? oItem1.title : "";
+				return sTitle.localeCompare(oItem2.title);
+			}).forEach(function(oItem) {
+				aSuggestionItems.push(new sap.m.SuggestionItem({
+					key: oItem.id,
+					text: oItem.title
+				}));
+			});
+			this._graph.setSearchSuggestionItems(aSuggestionItems);
+			oEvent.bPreventDefault = true;
+		},
+
+		onExit: function() {
+			if (this._oQuickView) {
+				this._oQuickView.destroy();
+			}
+		},
+
+		_setFilter: function() {
+			var aNodesCond = [],
+				aLinesCond = [];
+			var fnAddBossCondition = function(sBoss) {
+				aNodesCond.push(new sap.ui.model.Filter({
+					path: 'id',
+					operator: sap.ui.model.FilterOperator.EQ,
+					value1: sBoss
+				}));
+				aNodesCond.push(new sap.ui.model.Filter({
+					path: 'parent',
+					operator: sap.ui.model.FilterOperator.EQ,
+					value1: sBoss
+				}));
+			};
+			var fnAddLineCondition = function(sLine) {
+				aLinesCond.push(new sap.ui.model.Filter({
+					path: "from",
+					operator: sap.ui.model.FilterOperator.EQ,
+					value1: sLine
+				}));
+			};
+			this._mExplored.forEach(function(oItem) {
+				fnAddBossCondition(oItem);
+				fnAddLineCondition(oItem);
+			});
+			this._graph.getBinding("nodes").filter(new sap.ui.model.Filter({
+				filters: aNodesCond,
+				and: false
+			}));
+			this._graph.getBinding("lines").filter(new sap.ui.model.Filter({
+				filters: aLinesCond,
+				and: false
+			}));
+		},
+
+		_loadMore: function(sName) {
+			this._graph.deselect();
+			this._mExplored.push(sName);
+			this._graph.destroyAllElements();
+			this._setFilter();
+		},
+
+		_getCustomDataValue: function(oNode, sName) {
+			var aItems = oNode.getCustomData().filter(function(oData) {
+				return oData.getKey() === sName;
+			});
+			return aItems.length > 0 && aItems[0].getValue();
+		},
+
+		_openDetail: function(oNode, oButton) {
+			if (!this._oQuickView) {
+				this._oQuickView = sap.ui.xmlfragment("com.gv.hackathon.Hackathon5.view.Details", this);
+			}
+			this._oQuickView.setModel(new sap.ui.model.json.JSONModel({
+				icon: oNode.getImage() && oNode.getImage().getProperty("src"),
+				title: oNode.getDescription(),
+				location: this._getCustomDataValue(oNode, "location"),
+				itemDescription: this._getCustomDataValue(oNode, "description"),
+				productionPlant: this._getCustomDataValue(oNode, "production-plant"),
+				purchasePrice: this._getCustomDataValue(oNode, "purchase-price"),
+				estimatedTol: this._getCustomDataValue(oNode, "estimated-tol"),
+				status: this._getCustomDataValue(oNode, "status"),
+				temperature: this._getCustomDataValue(oNode, "temperature")
+			}));
+			jQuery.sap.delayedCall(0, this, function() {
+				this._oQuickView.openBy(oButton);
+			});
+		},
+
+		linePress: function(oEvent) {
+			oEvent.bPreventDefault = true;
 		}
 
 	});
